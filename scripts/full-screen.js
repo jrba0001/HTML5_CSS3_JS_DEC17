27bb16523a1fcffa6b9920b676e68ab63d8884f5
Sergio Fernández <sergio.fernandezj@gmail.com>
2018-01-18 22:56:45 +0100

    added full screen api

---
+++ scripts/full-screen.js
@@ -0,0 +1,30 @@
+var video = document.getElementById("video");
+
+video.addEventListener("click", function(event) {
+  launchFullScreen(this);
+  this.play();
+});
+
+function launchFullScreen(element) {
+  if (element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  } else if (element.msRequestFullScreen) {
+    element.msRequestFullScreen();
+  }
+}
+
+function exitFullScreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
